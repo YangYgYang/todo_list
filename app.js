b7d11ad62ecee6df4b5d5f9dc9437a6938a6e54a
YangYGYang <ginayoung82@gmail.com>
2022-06-25 12:18:55 +0800

    feat:add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose') // 載入 mongoose
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+const db = mongoose.connection
+
+//連線異常
+db.on('error', () => {
+    console.log('mongodb error!')
+})
+db.once('open', () => {
+    console.log('mongodb connected')
+})
+
+
+
 app.get('/', (req, res) => {
     res.send('todo list server')
 })
