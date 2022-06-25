b61d385885eb9ace02a9bf51c51250108833605e
YangYGYang <ginayoung82@gmail.com>
2022-06-25 16:35:44 +0800

    feat:modify index.hbs

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+//==========server setting
 const express = require('express')
 const app = express()
 const port = 3000
 
+//==========data base setting
+const Todo = require('./models/todo')
+
+app.get('/', (req, res) => {
+    Todo.find()
+        .lean()
+        .then(todos => res.render('index', { todos }))
+        .catch(error => console.error(error))
+})
+
+//==========ODM setting(use mongoose)
 const mongoose = require('mongoose') // 載入 mongoose
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
@@ -14,17 +26,19 @@ db.once('open', () => {
     console.log('mongodb connected')
 })
 
+//==========Template engine setting(use handlebars)
 const exphbs = require('express-handlebars')
-
-//extname是什麼意思？engine是指設定樣版引擎，所以代表我可以設定很多樣版引擎？但是網頁前台框架應該只會有一個？
+    //engine是指設定樣版引擎，所以代表我可以設定很多樣版引擎？但是網頁前台框架應該只會有一個？
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 
+//==========router setting
 app.get('/', (req, res) => {
     res.render('index')
 })
 
+//==========run server
 app.listen(port, () => {
     console.log(`Todo list server is running on http://localhost:${port}`)
 })
