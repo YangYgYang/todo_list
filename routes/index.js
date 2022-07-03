f19451003856732b93a29069ab228fb6ffffdc17
YangYGYang <ginayoung82@gmail.com>
2022-07-03 16:53:20 +0800

    seperate router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
+const todo = require('./modules/todo')
 
 router.use('/', home)
-
+router.use('/todos', todo)
 
 
 
