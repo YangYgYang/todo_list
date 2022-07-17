e8530b047165ca3e31e0ce19881ea342d112c258
YangYGYang <ginayoung82@gmail.com>
2022-07-17 20:27:53 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/login', (req, res) => {
-    res.render('login')
+        res.render('login')
+    })
+    //??為何到login頁面要用post
+router.post('/login', (req, res) => {})
+
+router.get('/register', (req, res) => {
+    res.render('register')
 })
+
+
 module.exports = router
